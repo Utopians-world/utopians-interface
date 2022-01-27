6cf495a95fc4af2b809e19a04e01d25e283fbb38
winddy <winddy.l@metis.io>
2022-01-28 00:23:30 +0800

    add leave page for url redirecting

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -3,15 +3,28 @@ import Dashboard from 'components/Dashboard'
 import Home from 'components/Home'
 import { HashRouter, Route, Switch } from 'react-router-dom'
 
+import { Modal as RouterModal } from 'antd'
+
 import CatchallRedirect from './CatchallRedirect'
 import Create from './Create'
 import Congratulation from './Create/Congratulation'
 import Projects from './Projects'
 import Faq from './Faq'
-
+const { confirm } = RouterModal
 export default function Router() {
   return (
-    <HashRouter>
+    <HashRouter
+      getUserConfirmation={(result, callback) => {
+        confirm({
+          className: 'continueModal',
+          title: <div className="continueModalTitle">Confirm</div>,
+          content: <div className="continueModalDes">{result}</div>,
+          okText: 'continue',
+          onOk: () => callback(true),
+          onCancel: () => callback(false),
+        })
+      }}
+    >
       <Switch>
         <Route exact path="/">
           <Home />
